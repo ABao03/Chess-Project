0ae0e99e477fd501361d2b54ca2f0579676c5634
Andrew-Bao <andrewbaoq3@gmail.com>
2022-10-31 12:35:21 -0400

    Co-authored-by: M0hamedd <M0hamedd@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from project import Chess_Game, Chess_Board
-import pygame, sys
+from project import Chess_Game, Chess_Board, Chess_Piece, Knight, Pawn
+import pygame
 from pygame.locals import *
 
 
@@ -8,5 +8,26 @@ while run:
     
     thisGame = Chess_Game()
     thisBoard = Chess_Board(thisGame)
-    pygame.display.update()
-    Chess_Game.Close_Game()
+    
+    Chess_Game.Close_Game() 
+
+    topLeftPawn = Pawn(thisGame)
+    topLeftPawn.drawPiece(50, 50)
+
+    topLeftKnight = Knight(thisGame)
+    topLeftKnight.drawPiece(150, 50)
+
+    bottomLeftKnight = Knight(thisGame)
+    bottomLeftKnight.drawPiece(150, 650)
+
+    topRightKnight = Knight(thisGame)
+    topRightKnight.drawPiece(600, 50)
+    
+    bottomRightKnight = Knight(thisGame)
+    bottomRightKnight.drawPiece(600, 650)
+    
+    
+    
+    pygame.display.update() # ALWAYS KEEP THIS AT THE END
+
+    
--- project.py
+++ project.py
@@ -39,10 +39,10 @@ class Chess_Game():
     def __init__(self) -> None:
         pygame.init()
         background = pygame.image.load("fidel.jpg")
-        SCREEN_WIDTH = 850
-        SCREEN_HEIGHT = 850
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.screen.blit(background, [0,0])
+        self.SCREEN_WIDTH = 850
+        self.SCREEN_HEIGHT = 850
+        self.thisScreen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        pygame.draw.rect(self.thisScreen, (255, 225, 219), pygame.Rect(0,0,self.SCREEN_WIDTH,self.SCREEN_HEIGHT)) 
     
     def Close_Game() -> None:
         for event in pygame.event.get():
@@ -50,70 +50,120 @@ class Chess_Game():
                 pygame.quit()
                 sys.exit()
 
-class Chess_Board(Chess_Game):
-    """
-    This class makes the board of the chess game
-
-    """
-    
-    def __init__(self, chess_game) -> None:
-        self.currentColor = [(255, 255, 255), (0,0,0)]
-        for i in range(7):
-            for j in range(7):
-                pygame.draw.rect(chess_game.screen, self.currentColor[(i+j)%2], pygame.Rect(100+125*j,100+125*i,125+125*j,125+125*i))
-
-class Chess_Piece():
+class Chess_Piece(Chess_Game):
     """
     This class makes the chess pieces do stuff
 
     """
-    def __init__(self) -> None:
-        self.displayImage
 
-    def drawPiece(image) -> None:
-        self.display
-        pass
+    def __init__(self, chess_game) -> None:
+        self.thisScreen = chess_game.screen
+
+    def drawPiece(self, x, y) -> pygame.image:
+        self.xCoord = x
+        self.yCoord = y
+
+        self.screen.blit(self.thisImage, (x,y))
 
     def takePiece() -> None:
         pass
 
+class Chess_Board(Chess_Game):
+    """
+    This class makes the board of the chess game. Board is simulated in a matrix
 
-class Pawn(Chess_Piece):
+    """
+    
+    def __init__(self, chess_game) -> None:
+        self.boardMap = {}
+        self.currentColor = [(255, 255, 255), (0,0,0)]
+        for i in range(8):
+            for j in range(8):
+                pygame.draw.rect(chess_game.thisScreen, self.currentColor[(i+j)%2], pygame.Rect(50+95*j,50+95*i,95,95)) 
+                # boardMap.add(i, j)
+
+class Pawn(Chess_Piece, Chess_Board, Chess_Game): 
     """
     This class implements the pawn piece (Chaff infantry)
 
     """
 
+    def __init__(self, game) -> None:
+        super()
+        self.screen = game.thisScreen
+        self.thisImage = pygame.image.load("pawn.jpg")
+        self.thisImage = pygame.transform.scale(self.thisImage, (95, 95)) 
 
-class King(Chess_Piece):
+
+class King(Chess_Piece, Chess_Board, Chess_Game):
     """
     This class implements the king piece (Legendary lord) 
 
     """
+    def __init__(self, game) -> None:
+        super()
+        self.screen = game.thisScreen
+        self.thisImage = pygame.image.load("king.jpg")
+        self.thisImage = pygame.transform.scale(self.thisImage, (95, 95))
 
-class Queen(Chess_Piece):
+class Queen(Chess_Piece, Chess_Board, Chess_Game):
     """
     This class implements the queen piece (Hero)
 
     """
+    def __init__(self, game) -> None:
+        super()
+        self.screen = game.thisScreen
+        self.thisImage = pygame.image.load("queen.jpg")
+        self.thisImage = pygame.transform.scale(self.thisImage, (95, 95))
 
-class Bishop(Chess_Piece):
+class Bishop(Chess_Piece, Chess_Board, Chess_Game): 
     """
     This class implements the bishop piece (Mage)
 
     """
+    def __init__(self, game) -> None:
+            super()
+            self.screen = game.thisScreen
+            self.thisImage = pygame.image.load("bishop.jpg")
+            self.thisImage = pygame.transform.scale(self.thisImage, (95, 95))
 
-class Knight(Chess_Piece):
+class Knight(Chess_Piece, Chess_Board, Chess_Game):
     """
     This class implements the knight piece (Cavalry)
     
     """
-    def __init__(self) -> None:
-        super()
-        displayImage = pygame.image.load("Chess_Horse.jpg")
+        # def __init__(self, chess_game) -> None:
+        # self.currentColor = [(255, 255, 255), (0,0,0)] 
+        # for i in range(8): 
+        #     for j in range(8):
+        #         pygame.draw.rect(chess_game.thisScreen, self.currentColor[(i+j)%2], pygame.Rect(50+95*j,50+95*i,95,95)) 
 
-class Rook(Chess_Piece):
+    def __init__(self, game) -> None:
+        super()
+        self.screen = game.thisScreen
+        self.thisImage = pygame.image.load("knight.jpg")
+        self.thisImage = pygame.transform.scale(self.thisImage, (95, 95))
+
+class Rook(Chess_Piece, Chess_Board, Chess_Game):
     """
-    This class implements the rook piece ()
+    This class implements the rook piece (Siege Tower)
     
+    """
+    def __init__(self, game) -> None:
+        super()
+        self.screen = game.thisScreen
+        self.thisImage = pygame.image.load("rook.jpg")
+        self.thisImage = pygame.transform.scale(self.thisImage, (95, 95))
+
+class TakenPieces():
+    """
+    This class displays the game's casualties
+
+    """
+
+class Timer():
+    """
+    This class displays the time passed when each black and white take their turn to choose their move 
+
     """
